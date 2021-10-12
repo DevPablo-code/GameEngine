fa7da8d137ef8c2cf74984dfe68ab9421f2788e3
DevPablo-code <61848330+DevPablo-code@users.noreply.github.com>
2021-10-12 21:39:44 +0300

    Scene Manager

--- Managers/SceneManager.js
+++ Managers/SceneManager.js
@@ -1,6 +1,46 @@
-class SceneManager {
-  update() {
+class SceneObject {
+  position;
+  abstract;
+  callback;
 
+  changeCallback(callback) {
+    this.callback = callback;
+  }
+
+  constructor(position, callback = () => {}, abstract) {
+    this.callback = callback;
+    this.abstract = Boolean(abstract);
+    if (!this.abstract)
+      this.position = position;
+  }
+}
+
+class SceneManager {
+  objects = []; // vector<SceneObject>;
+
+  addObject(object) {
+    if (object instanceof SceneObject) {
+      this.objects.push(object);
+    } else {
+      console.log(`${object.constructor.name} is not instance of SceneObject`);
+    }
+  }
+
+  setObjects(objects) {
+    this.objects = [];
+    objects.forEach(object => {
+      if (object instanceof SceneObject) {
+        this.objects.push(object);
+      } else {
+        console.log(`${object.constructor.name} is not instance of SceneObject`);
+      }
+    })
+  }
+
+  update() {
+    this.objects.forEach(object => {
+      object.callback();
+    })
   }
 }
 
