8556213bc95b67c90b7f7b47ac491b9879687810
DevPablo-code <61848330+DevPablo-code@users.noreply.github.com>
2021-10-19 21:10:43 +0300

    Events manager

--- Engine.js
+++ Engine.js
@@ -2,8 +2,10 @@ const InputManager = require('./Managers/InputManager');
 const { SceneManager } = require('./Managers/SceneManager');
 const { RenderManager } = require('./Managers/RenderManager');
 const AssetsManager = require('./Managers/AssetsManager');
+const { EventsManager } = require('./Managers/EventsManager');
 
 class Engine {
+  eventsManager = new EventsManager();
   inputManager = new InputManager();
   renderManager = new RenderManager();
   sceneManager = new SceneManager();
---
+++ Managers/EventsManager.js
@@ -0,0 +1,81 @@
+class Event {
+  category ;
+  subcategory;
+  action;
+  args;
+
+  constructor(category = '', subcategory = '', action = '', args = {}) {
+    this.category = category;
+    this.subcategory = subcategory;
+    this.action = action;
+    this.args = args;
+  }
+}
+
+class KeyboardEvent extends Event {
+  constructor(action, args) {
+    super('Keyboard', action, args);
+  }
+}
+
+class MouseEvent extends Event {
+  constructor(subcategory, action, args) {
+    super('Mouse', subcategory, action, args);
+  }
+}
+
+class LevelEvent extends Event {
+  constructor(subcategory, action, args) {
+    super('Level', subcategory, action, args);
+  }
+}
+
+class WindowEvent extends Event {
+  constructor(subcategory, action, args) {
+    super('Window', subcategory, action, args);
+  }
+}
+
+class EventsManager {   
+  events;
+
+  constructor() {
+    this.events = new Map();
+  }
+
+  addListener(event, callback) {
+    if (!this.events.has(event)) {  
+      this.events.set(event, []);
+    }
+    const listeners = this.events.get(event);
+    return listeners.push(callback) - 1;
+  }
+
+  removeListener(event, listenerIndex) {
+    if (this.events.has(event)) {
+      const listeners = this.events.get(event);
+      listeners.splice(listenerIndex, 1);
+    }
+  }
+
+  removeListeners(event) {
+    this.events.delete(event);
+  }
+
+  clearEvents() {
+    this.events.clear();
+  }
+
+  getEventListeners(event) {
+    return this.events.get(event) || [];
+  }
+
+  emit(event) {
+    const listeners = [...this.getEventListeners(event.category), ...this.getEventListeners(`${event.category}.${event.subcategory}`), ...this.getEventListeners(`${event.category}.${event.subcategory}.${event.action}`)];
+    listeners.forEach((listener) => {
+      listener();
+    })
+  }
+}
+
+module.exports = { EventsManager, Event, KeyboardEvent, MouseEvent, LevelEvent, WindowEvent };
