c67bbe630dcbe4c17c081204eac8dd436a615846
DevPablo-code <61848330+DevPablo-code@users.noreply.github.com>
2021-10-29 18:19:36 +0300

    buildLevelRender moved to SceneManager

--- Managers/SceneManager.js
+++ Managers/SceneManager.js
@@ -1,47 +1,47 @@
-class SceneObject {
-  position;
-  abstract;
-  callback;
-
-  changeCallback(callback) {
-    this.callback = callback;
-  }
-
-  constructor(position, callback = () => {}, abstract) {
-    this.callback = callback;
-    this.abstract = Boolean(abstract);
-    if (!this.abstract)
-      this.position = position;
-  }
-}
+const YAML = require('js-yaml');
+const { RenderTarget } = require('../Managers/RenderManager')
 
 class SceneManager {
-  objects = []; // vector<SceneObject>;
-
-  addObject(object) {
-    if (object instanceof SceneObject) {
-      this.objects.push(object);
-    } else {
-      console.log(`${object.constructor.name} is not instance of SceneObject`);
-    }
+  constructor(engine) {
+    this.engine = engine;
   }
-
-  setObjects(objects) {
-    this.objects = [];
-    objects.forEach(object => {
-      if (object instanceof SceneObject) {
-        this.objects.push(object);
-      } else {
-        console.log(`${object.constructor.name} is not instance of SceneObject`);
+  async buildLevelRender(text) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const parsed = YAML.load(text);
+        if (parsed.renderObjects && Array.isArray(parsed.renderObjects)) {
+             const renderTargets = [];
+             for (let renderObject of parsed.renderObjects) {
+              const renderTarget = new RenderTarget();
+              if (renderObject.position) {
+                renderTarget.setPosition(renderObject.position);
+              }
+              if (renderObject.rotation) {
+                renderTarget.setRotation(renderObject.rotation);
+              }
+              if (renderObject.size) {
+                renderTarget.setSize(renderObject.size);
+              }
+              if (renderObject.imagePath) {
+                const imageAssetId = this.engine.assetsManager.addAsset(renderObject.imagePath);
+                await this.engine.assetsManager.loadAssets();
+                const imageAsset = this.engine.assetsManager.getAsset(imageAssetId);
+                renderTarget.setImage(imageAsset);          
+              }
+              if (renderObject.mirror) {
+                renderTarget.setMirror(renderObject.mirror);
+              }
+              renderTargets.push(renderTarget);
+             }
+             resolve(renderTargets);
+        } else {
+          reject('Invalid format. No "renderObjects" key or it isnt array')
+        }
+      } catch (err) {
+        reject(err);
       }
     })
   }
-
-  update() {
-    this.objects.forEach(object => {
-      object.callback();
-    })
-  }
 }
 
-module.exports = { SceneManager, SceneObject };
+module.exports = { SceneManager };
